c5e06ce7e3aec42a88a996894a527203ca2af787
stanly <stanlyjohnson@outlook.com>
2017-09-22 14:20:08 +0530

    cleanup-organise
    organised storyboard files into groups

--- Reubro_Project_Tracker/PeopleTableViewCell.swift
+++ Reubro_Project_Tracker/PeopleTableViewCell.swift
@@ -1,26 +1,20 @@
 //
-//  HomeTableViewCell.swift
+//  PeopleTableViewCell.swift
 //  Reubro_Project_Tracker
 //
-//  Created by reubro on 20/09/17.
+//  Created by reubro on 22/09/17.
 //  Copyright © 2017 Reubro. All rights reserved.
 //
 
 import UIKit
 
-class HomeTableViewCell: UITableViewCell {
+class PeopleTableViewCell: UITableViewCell {
 
-    
-    @IBOutlet weak var nameStatusOfProject: UILabel!
-
-
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-    
-    
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
